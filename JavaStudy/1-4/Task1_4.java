64fcd68e0b58a7f9772a9a9b63e48894034b4254
koskakento <hyahhaaaa.425@outlook.jp>
2020-08-24 16:46:40 +0900

    課提出
    Task1_4の提出

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -9,8 +9,8 @@ public class Task1_4 {
 
     // 定数（条件）
     // ① 名前とパスワードの定数を定義してください。
-String sh = "パスワード";
-
+    private static final String CONST_ALICE = "Alice";
+    private static final String CONST_PASS = "pass";
     // 補足:
     //   定数は②〜⑤で使い回すために定義しておきます。
     //   final修飾子を付与すると、変数（動的に変わる値）ではなく、定数（変更不可な定まった値）となります。
@@ -30,23 +30,23 @@ String sh = "パスワード";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
 if (name.equals("alice")){
-System.out.println("ログイン成功です。");
+System.out.println(CONST_MSG_SUCCESS);
 }
 
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
 else if (name.equals("pass")){
-System.out.println("パスワードに誤りがあります。");
+System.out.println(CONST_MSG_ERROR_PASS);
 
 }
 
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
 else if (pass.equals("pass")){
-System.out.println("名前に誤りがあります。");
+System.out.println(CONST_MSG_ERROR_NAME);
 
 
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
 } else {
-    System.out.println("入力情報に誤りがあります。");
+    System.out.println(CONST_MSG_ERROR_INPUT);
 }
     }
 
